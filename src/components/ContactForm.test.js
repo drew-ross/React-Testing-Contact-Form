a41b205b471cee9e21ddff1dd33a520fa07e8eff
Drew Ross <mail.drew.ross@gmail.com>
2020-07-09 13:34:05 -0700

    Create test to confirm ContatForm takes input and displays input in input fields.

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -7,15 +7,26 @@ test('ContactForm renders without errors', () => {
     render(<ContactForm />);
 });
 
-test('ContactForm takes input and renders expected output', () => {
+test('ContactForm takes input', () => {
     /// Arrange
     render(<ContactForm />);
 
     /// Act
+    // get inputs
     const fNameInput = screen.getByLabelText(/first name/i);
     const lNameInput = screen.getByLabelText(/last name/i);
     const emailInput = screen.getByLabelText(/email/i);
     const messageInput = screen.getByLabelText(/message/i);
 
+    // input text
+    fireEvent.change(fNameInput, { target: { value: 'John' } });
+    fireEvent.change(lNameInput, { target: { value: 'Smith' } });
+    fireEvent.change(emailInput, { target: { value: 'js@email.com' } });
+    fireEvent.change(messageInput, { target: { value: 'Hello World!' } });
+
     /// Assert
-})
+    screen.getByDisplayValue(/john/i);
+    screen.getByDisplayValue(/smith/i);
+    screen.getByDisplayValue(/js@email.com/i);
+    screen.getByDisplayValue(/Hello World!/i);
+});
